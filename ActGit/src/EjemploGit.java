15413eda73bbca8ad7eeaf3814414fe2b08e5838
Daniel Sempere <sempere.dani@gmail.com>
2014-01-08 23:52:41 +0100

    Añadimos las modificaciones

--- ActGit/src/EjemploGit.java
+++ ActGit/src/EjemploGit.java
@@ -3,8 +3,8 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Act GitHub1 sin modificar");
-		
+		System.out.println("Act GitHub1 modificado");
+		System.out.println("compartir repositorio");
 	}
 
 }
